6b06db55de1e4c324165038602dd7d6956e5b0a6
DaDevMate <67144384+dev365b@users.noreply.github.com>
2023-03-09 07:01:20 +0800

    Initial Commit

---
+++ Popup view app/Popup_view_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Popup_view_appApp.swift
+//  Popup view app
+//
+//  Created by Nethan on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Popup_view_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
