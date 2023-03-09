48c66922a1ef23ada094ccb6c8a1670a0f57fbd5
DaDevMate <67144384+dev365b@users.noreply.github.com>
2023-03-09 12:15:01 +0800

    first

--- Popup view app/ContentView.swift
+++ Popup view app/ContentView.swift
@@ -6,16 +6,80 @@
 //
 
 import SwiftUI
+import SceneKit
 
 struct ContentView: View {
+    @State var clicked = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+   
+        
+        Button {
+            clicked = true
+        } label: {
+            Text("Click")
         }
-        .padding()
+        .sheet(isPresented: $clicked) {
+            VStack {
+                VStack {
+                    Spacer()
+                    Text("Nethan's AirPods Max")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                    Spacer()
+                }
+             
+                SceneKitView(scene: SCNScene(named: "airpods-max.usdz")!)
+                    .edgesIgnoringSafeArea(.all)
+                Spacer()
+                
+                Image(systemName: "battery.75")
+                    .foregroundColor(.green)
+                    .font(.title)
+                Spacer()
+                Text("75%")
+                    .foregroundColor(.black)
+                Spacer()
+                Spacer()
+                
+            }
+            .background(Color.white)
+            .presentationDetents([.height(350)])
+        }
+        
+    }
+}
+
+struct SceneKitView: UIViewRepresentable {
+    let scene: SCNScene
+    let cameraNode = SCNNode()
+
+    func makeUIView(context: Context) -> SCNView {
+        let scnView = SCNView()
+        scnView.scene = scene
+        scnView.backgroundColor = UIColor.white
+        scnView.autoenablesDefaultLighting = true
+        scnView.allowsCameraControl = true
+        
+        // Set up camera
+        let camera = SCNCamera()
+        cameraNode.camera = camera
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 5)
+        scnView.pointOfView = cameraNode
+        
+        let boundingBox = scene.rootNode.boundingBox
+                let center = SCNVector3(
+                    x: (boundingBox.max.x - boundingBox.min.x) / 2 + boundingBox.min.x,
+                    y: (boundingBox.max.y - boundingBox.min.y) / 2 + boundingBox.min.y,
+                    z: (boundingBox.max.z - boundingBox.min.z) / 2 + boundingBox.min.z
+                )
+                scene.rootNode.position = SCNVector3(x: -center.x, y: -center.y, z: -center.z)
+                
+        
+        return scnView
+    }
+
+    func updateUIView(_ scnView: SCNView, context: Context) {
     }
 }
 
